96a0aedc88bb223df1ab6565c3d53a174a87ce79
Ryan Giordano <rgiorda1@gmail.com>
2016-10-24 23:49:54 -0400

    added register

--- src/app/auth/register-form/register-form.component.ts
+++ src/app/auth/register-form/register-form.component.ts
@@ -10,10 +10,13 @@ export class RegisterFormComponent implements OnInit {
   form:FormGroup;
   constructor(private _authService: AuthService, private formBuilder: FormBuilder ) {
     this.form = this.formBuilder.group({
-
+      'email': ['', Validators.required],
+      'password': ['', Validators.required]
     })
   }
-
+  isErrorVisible(field:string, error:string){
+    return this.form.controls[field].dirty && this.form.controls[field].errors && this.form.controls[field].errors[error];
+  }
   ngOnInit() {
   }
 
